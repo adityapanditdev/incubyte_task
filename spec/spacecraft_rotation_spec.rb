79d4e4aadc4968f38b16c9180eefc31393e3bd06
aditya pandit <adityapanditdev@gmail.com>
2024-01-24 12:06:13 +0530

    added spec for rotation of spacecraft

---
+++ spec/spacecraft_rotation_spec.rb
@@ -0,0 +1,18 @@
+
+RSpec.describe SpacecraftRotation do
+  let(:dummy_class) { Class.new { include SpacecraftRotation } }
+
+  describe '#turn_left' do
+    it 'turns left from North direction' do
+      instance = dummy_class.new
+      expect(instance.turn_left('N', nil)).to eq('W')
+    end
+  end
+  
+  describe '#turn_right' do
+    it 'turns right from North direction' do
+      instance = dummy_class.new
+      expect(instance.turn_right('N', nil)).to eq('E')
+    end
+  end
+end
